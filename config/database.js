2a4043b7520a624868ed6265b708723194df1cfe
Nathan Toombs <ntoombs19@gmail.com>
2023-10-19 16:47:58 -0500

    Fix database configuration for local strapi instance

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: {
+      ssl: env.bool('DATABASE_SSL', false) && {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
     },
