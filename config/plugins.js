841398ba9248083cbd8dd8bffbf68296dbadaa87
Nathan Toombs <nathan.toombs@congenius.com>
2023-10-29 10:16:24 -0500

    Update image security policy

--- config/plugins.js
+++ config/plugins.js
@@ -8,18 +8,17 @@ module.exports = ({ env }) => ({
           secretAccessKey: env('AWS_ACCESS_SECRET'),
           region: env('AWS_REGION'),
           params: {
+            ACL: 'private',
+            signedUrlExpires: 15 * 60,
             Bucket: env('AWS_BUCKET_NAME'),
           },
         }
       },
       // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
       actionOptions: {
-        upload: {
-          ACL: null
-        },
-        uploadStream: {
-          ACL: null
-        },
+        upload: {},
+        uploadStream: {},
+        delete: {},
       }
     },
   },
